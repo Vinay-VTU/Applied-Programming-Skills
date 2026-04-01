b58c39c03d1b88075ebd52aefa0d20b6d6d03ab0
Vinay-VTU <vtu25539@veltech.edu.in>
2026-04-01 10:05:49 +0530

    Time: 3 ms (82.43%), Space: 48.5 MB (29.23%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+        for (char task : tasks) {
+            freq[task - 'A']++;
+        }
+
+        Arrays.sort(freq);
+        int maxFreq = freq[25];
+        int maxCount = 0;
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == maxFreq) maxCount++;
+            else break;
+        }
+
+        int partCount = maxFreq - 1;
+        int partLength = n + 1;
+        int emptySlots = partCount * partLength + maxCount;
+
+        return Math.max(emptySlots, tasks.length);
+    }
+}
