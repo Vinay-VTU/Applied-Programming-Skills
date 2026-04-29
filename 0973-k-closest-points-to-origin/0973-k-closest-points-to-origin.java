a1551165301d3f103efd19e5bad1c7fb87d86f4c
Vinay-VTU <vtu25539@veltech.edu.in>
2026-04-29 18:03:30 +0530

    Time: 26 ms (75.54%), Space: 55.6 MB (40.28%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        Arrays.sort(points, (a, b) -> {
+            int distA = a[0] * a[0] + a[1] * a[1];
+            int distB = b[0] * b[0] + b[1] * b[1];
+            return distA - distB;
+        });
+
+        int[][] ans = new int[k][2];
+
+        for (int i = 0; i < k; i++) {
+            ans[i] = points[i];
+        }
+
+        return ans;
+    }
+}
