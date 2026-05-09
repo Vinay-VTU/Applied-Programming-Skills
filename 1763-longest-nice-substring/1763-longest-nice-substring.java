2cebcea0bcab79b0458cafb22079b99007b5289b
Vinay-VTU <vtu25539@veltech.edu.in>
2026-05-09 22:24:24 +0530

    Time: 22 ms (25.67%), Space: 46.7 MB (16.04%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.java
+++ 1763-longest-nice-substring/1763-longest-nice-substring.java
@@ -1,35 +1,35 @@
 class Solution {
-    public String longestNiceSubstring(String s) {
-        String ans = "";
+        public String longestNiceSubstring(String s) {
+                String answer = "";
 
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j <= s.length(); j++) {
-                String sub = s.substring(i, j);
+                        for (int i = 0; i < s.length(); i++) {
+                                    for (int j = i + 1; j <= s.length(); j++) {
+                                                    String sub = s.substring(i, j);
 
-                if (isNice(sub) && sub.length() > ans.length()) {
-                    ans = sub;
-                }
-            }
-        }
+                                                                    if (isNice(sub) && sub.length() > answer.length()) {
+                                                                                        answer = sub;
+                                                                                                        }
+                                                                                                                    }
+                                                                                                                            }
 
-        return ans;
-    }
+                                                                                                                                    return answer;
+                                                                                                                                        }
 
-    private boolean isNice(String str) {
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
+                                                                                                                                            private boolean isNice(String str) {
+                                                                                                                                                    for (int i = 0; i < str.length(); i++) {
+                                                                                                                                                                char ch = str.charAt(i);
 
-            if (Character.isLowerCase(ch)) {
-                if (str.indexOf(Character.toUpperCase(ch)) == -1) {
-                    return false;
-                }
-            } else {
-                if (str.indexOf(Character.toLowerCase(ch)) == -1) {
-                    return false;
-                }
-            }
-        }
+                                                                                                                                                                            if (Character.isLowerCase(ch)) {
+                                                                                                                                                                                            if (str.indexOf(Character.toUpperCase(ch)) == -1) {
+                                                                                                                                                                                                                return false;
+                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                            } else {
+                                                                                                                                                                                                                                                            if (str.indexOf(Character.toLowerCase(ch)) == -1) {
+                                                                                                                                                                                                                                                                                return false;
+                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                                    }
 
-        return true;
-    }
-}
+                                                                                                                                                                                                                                                                                                                            return true;
+                                                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                                                }
