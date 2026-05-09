69f12f8a2d31b84b29b685867103153ef16521ec
Vinay-VTU <vtu25539@veltech.edu.in>
2026-05-09 21:59:54 +0530

    Time: 2 ms (90.39%), Space: 44.1 MB (71.52%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -2,56 +2,59 @@ import java.util.*;
 
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+            int m = grid.length;
+                    int n = grid[0].length;
 
-        Queue<int[]> q = new LinkedList<>();
-        int fresh = 0;
+                            Queue<int[]> q = new LinkedList<>();
+                                    int fresh = 0;
 
-        // Add all rotten oranges to queue
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2) {
-                    q.add(new int[]{i, j});
-                } else if (grid[i][j] == 1) {
-                    fresh++;
-                }
-            }
-        }
+                                            // Add all rotten oranges to queue
+                                                    for (int i = 0; i < m; i++) {
+                                                                for (int j = 0; j < n; j++) {
+                                                                                if (grid[i][j] == 2) {
+                                                                                                    q.add(new int[]{i, j});
+                                                                                                                    } else if (grid[i][j] == 1) {
+                                                                                                                                        fresh++;
+                                                                                                                                                        }
+                                                                                                                                                                    }
+                                                                                                                                                                            }
 
-        if (fresh == 0) return 0;
+                                                                                                                                                                                    if (fresh == 0) {
+                                                                                                                                                                                                return 0;
+                                                                                                                                                                                                        }
 
-        int minutes = 0;
-        int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+                                                                                                                                                                                                                int minutes = 0;
+                                                                                                                                                                                                                        int[][] dirs = {
+                                                                                                                                                                                                                                    {1, 0}, {-1, 0}, {0, 1}, {0, -1}
+                                                                                                                                                                                                                                            };
 
-        // BFS level by level
-        while (!q.isEmpty()) {
-            int size = q.size();
-            boolean rottenThisMinute = false;
+                                                                                                                                                                                                                                                    while (!q.isEmpty()) {
+                                                                                                                                                                                                                                                                int size = q.size();
+                                                                                                                                                                                                                                                                            boolean rotted = false;
 
-            for (int k = 0; k < size; k++) {
-                int[] cell = q.poll();
-                int r = cell[0];
-                int c = cell[1];
+                                                                                                                                                                                                                                                                                        for (int i = 0; i < size; i++) {
+                                                                                                                                                                                                                                                                                                        int[] cell = q.poll();
+                                                                                                                                                                                                                                                                                                                        int row = cell[0];
+                                                                                                                                                                                                                                                                                                                                        int col = cell[1];
 
-                for (int[] d : dirs) {
-                    int nr = r + d[0];
-                    int nc = c + d[1];
+                                                                                                                                                                                                                                                                                                                                                        for (int[] d : dirs) {
+                                                                                                                                                                                                                                                                                                                                                                            int nr = row + d[0];
+                                                                                                                                                                                                                                                                                                                                                                                                int nc = col + d[1];
 
-                    if (nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1) {
-                        grid[nr][nc] = 2;
-                        fresh--;
-                        q.add(new int[]{nr, nc});
-                        rottenThisMinute = true;
-                    }
-                }
-            }
+                                                                                                                                                                                                                                                                                                                                                                                                                    if (nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1) {
+                                                                                                                                                                                                                                                                                                                                                                                                                                            grid[nr][nc] = 2;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                    fresh--;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            q.add(new int[]{nr, nc});
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    rotted = true;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    }
 
-            if (rottenThisMinute) {
-                minutes++;
-            }
-        }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                if (rotted) {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                minutes++;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    }
 
-        return fresh == 0 ? minutes : -1;
-    }
-}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            return fresh == 0 ? minutes : -1;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
