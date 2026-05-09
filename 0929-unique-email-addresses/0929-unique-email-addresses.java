3517461f429206be1d41ab45ca9175a526b921ee
Vinay-VTU <vtu25539@veltech.edu.in>
2026-05-09 22:15:15 +0530

    Time: 17 ms (16.45%), Space: 46.3 MB (85.33%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.java
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -2,32 +2,32 @@ import java.util.*;
 
 class Solution {
     public int numUniqueEmails(String[] emails) {
-        HashSet<String> set = new HashSet<>();
+            HashSet<String> set = new HashSet<>();
 
-        for (String email : emails) {
-            String[] parts = email.split("@");
+                    for (String email : emails) {
+                                String[] parts = email.split("@");
 
-            String local = parts[0];
-            String domain = parts[1];
+                                            String local = parts[0];
+                                                        String domain = parts[1];
 
-            StringBuilder clean = new StringBuilder();
+                                                                    StringBuilder cleanLocal = new StringBuilder();
 
-            for (int i = 0; i < local.length(); i++) {
-                char ch = local.charAt(i);
+                                                                                for (int i = 0; i < local.length(); i++) {
+                                                                                                char ch = local.charAt(i);
 
-                if (ch == '+') {
-                    break;
-                }
+                                                                                                                if (ch == '+') {
+                                                                                                                                    break;
+                                                                                                                                                    }
 
-                if (ch != '.') {
-                    clean.append(ch);
-                }
-            }
+                                                                                                                                                                    if (ch != '.') {
+                                                                                                                                                                                        cleanLocal.append(ch);
+                                                                                                                                                                                                        }
+                                                                                                                                                                                                                    }
 
-            String finalEmail = clean.toString() + "@" + domain;
-            set.add(finalEmail);
-        }
+                                                                                                                                                                                                                                String finalEmail = cleanLocal.toString() + "@" + domain;
+                                                                                                                                                                                                                                            set.add(finalEmail);
+                                                                                                                                                                                                                                                    }
 
-        return set.size();
-    }
-}
+                                                                                                                                                                                                                                                            return set.size();
+                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                }
