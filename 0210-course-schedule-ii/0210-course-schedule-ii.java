163471639c929173485c870061f89aeb04e2a6a0
Vinay-VTU <vtu25539@veltech.edu.in>
2026-05-09 22:05:03 +0530

    Time: 5 ms (84.08%), Space: 46.8 MB (56.35%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -2,50 +2,49 @@ import java.util.*;
 
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        int[] indegree = new int[numCourses];
+            ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+                    int[] indegree = new int[numCourses];
 
-        for (int i = 0; i < numCourses; i++) {
-            graph.add(new ArrayList<>());
-        }
+                            for (int i = 0; i < numCourses; i++) {
+                                        graph.add(new ArrayList<>());
+                                                }
 
-        // [a, b] means b must come before a => b -> a
-        for (int[] p : prerequisites) {
-            int course = p[0];
-            int pre = p[1];
+                                                        for (int[] pre : prerequisites) {
+                                                                    int course = pre[0];
+                                                                                int prereq = pre[1];
 
-            graph.get(pre).add(course);
-            indegree[course]++;
-        }
+                                                                                            graph.get(prereq).add(course);
+                                                                                                        indegree[course]++;
+                                                                                                                }
 
-        Queue<Integer> q = new LinkedList<>();
+                                                                                                                        Queue<Integer> queue = new LinkedList<>();
 
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.add(i);
-            }
-        }
+                                                                                                                                for (int i = 0; i < numCourses; i++) {
+                                                                                                                                            if (indegree[i] == 0) {
+                                                                                                                                                            queue.add(i);
+                                                                                                                                                                        }
+                                                                                                                                                                                }
 
-        int[] order = new int[numCourses];
-        int index = 0;
+                                                                                                                                                                                        int[] order = new int[numCourses];
+                                                                                                                                                                                                int index = 0;
 
-        while (!q.isEmpty()) {
-            int current = q.poll();
-            order[index++] = current;
+                                                                                                                                                                                                        while (!queue.isEmpty()) {
+                                                                                                                                                                                                                    int current = queue.poll();
+                                                                                                                                                                                                                                order[index++] = current;
 
-            for (int next : graph.get(current)) {
-                indegree[next]--;
+                                                                                                                                                                                                                                            for (int next : graph.get(current)) {
+                                                                                                                                                                                                                                                            indegree[next]--;
 
-                if (indegree[next] == 0) {
-                    q.add(next);
-                }
-            }
-        }
+                                                                                                                                                                                                                                                                            if (indegree[next] == 0) {
+                                                                                                                                                                                                                                                                                                queue.add(next);
+                                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                                                    }
 
-        if (index == numCourses) {
-            return order;
-        }
+                                                                                                                                                                                                                                                                                                                                            if (index == numCourses) {
+                                                                                                                                                                                                                                                                                                                                                        return order;
+                                                                                                                                                                                                                                                                                                                                                                }
 
-        return new int[0];
-    }
-}
+                                                                                                                                                                                                                                                                                                                                                                        return new int[0];
+                                                                                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                                                                                            }
