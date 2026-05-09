1b7b6f9e554c46e081fbd3a71a0a054fa6ddc61a
Vinay-VTU <vtu25539@veltech.edu.in>
2026-05-09 22:02:46 +0530

    Time: 1 ms (85.83%), Space: 45.8 MB (97.26%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -2,26 +2,28 @@ import java.util.*;
 
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        boolean[] visited = new boolean[rooms.size()];
+            boolean[] visited = new boolean[rooms.size()];
 
-        dfs(rooms, 0, visited);
+                    dfs(rooms, 0, visited);
 
-        for (boolean room : visited) {
-            if (!room) {
-                return false;
-            }
-        }
+                            for (boolean v : visited) {
+                                        if (!v) {
+                                                        return false;
+                                                                    }
+                                                                            }
 
-        return true;
-    }
+                                                                                    return true;
+                                                                                        }
 
-    private void dfs(List<List<Integer>> rooms, int room, boolean[] visited) {
-        visited[room] = true;
+                                                                                            private void dfs(List<List<Integer>> rooms, int room, boolean[] visited) {
+                                                                                                    if (visited[room]) {
+                                                                                                                return;
+                                                                                                                        }
 
-        for (int key : rooms.get(room)) {
-            if (!visited[key]) {
-                dfs(rooms, key, visited);
-            }
-        }
-    }
-}
+                                                                                                                                visited[room] = true;
+
+                                                                                                                                        for (int key : rooms.get(room)) {
+                                                                                                                                                    dfs(rooms, key, visited);
+                                                                                                                                                            }
+                                                                                                                                                                }
+                                                                                                                                                                }
