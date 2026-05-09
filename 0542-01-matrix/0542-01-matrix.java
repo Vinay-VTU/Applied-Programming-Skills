7968ea854f70b8bfc49a697a5aa312a137d3a4a9
Vinay-VTU <vtu25539@veltech.edu.in>
2026-05-09 22:07:40 +0530

    Time: 13 ms (91.24%), Space: 49.3 MB (39.44%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -2,48 +2,46 @@ import java.util.*;
 
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
+            int m = mat.length;
+                    int n = mat[0].length;
 
-        int[][] dist = new int[m][n];
-        boolean[][] visited = new boolean[m][n];
-        Queue<int[]> q = new LinkedList<>();
+                            Queue<int[]> queue = new LinkedList<>();
+                                    boolean[][] visited = new boolean[m][n];
 
-        // Add all 0 cells first
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    q.add(new int[]{i, j});
-                    visited[i][j] = true;
-                }
-            }
-        }
+                                            // Add all 0 cells to queue
+                                                    for (int i = 0; i < m; i++) {
+                                                                for (int j = 0; j < n; j++) {
+                                                                                if (mat[i][j] == 0) {
+                                                                                                    queue.add(new int[]{i, j});
+                                                                                                                        visited[i][j] = true;
+                                                                                                                                        }
+                                                                                                                                                    }
+                                                                                                                                                            }
 
-        int[][] dirs = {
-            {1, 0},
-            {-1, 0},
-            {0, 1},
-            {0, -1}
-        };
+                                                                                                                                                                    int[][] dirs = {
+                                                                                                                                                                                {1, 0},
+                                                                                                                                                                                            {-1, 0},
+                                                                                                                                                                                                        {0, 1},
+                                                                                                                                                                                                                    {0, -1}
+                                                                                                                                                                                                                            };
 
-        // BFS from all 0 cells
-        while (!q.isEmpty()) {
-            int[] cell = q.poll();
-            int r = cell[0];
-            int c = cell[1];
+                                                                                                                                                                                                                                    while (!queue.isEmpty()) {
+                                                                                                                                                                                                                                                int[] cell = queue.poll();
+                                                                                                                                                                                                                                                            int row = cell[0];
+                                                                                                                                                                                                                                                                        int col = cell[1];
 
-            for (int[] d : dirs) {
-                int nr = r + d[0];
-                int nc = c + d[1];
+                                                                                                                                                                                                                                                                                    for (int[] d : dirs) {
+                                                                                                                                                                                                                                                                                                    int nr = row + d[0];
+                                                                                                                                                                                                                                                                                                                    int nc = col + d[1];
 
-                if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc]) {
-                    dist[nr][nc] = dist[r][c] + 1;
-                    visited[nr][nc] = true;
-                    q.add(new int[]{nr, nc});
-                }
-            }
-        }
+                                                                                                                                                                                                                                                                                                                                    if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc]) {
+                                                                                                                                                                                                                                                                                                                                                        mat[nr][nc] = mat[row][col] + 1;
+                                                                                                                                                                                                                                                                                                                                                                            visited[nr][nc] = true;
+                                                                                                                                                                                                                                                                                                                                                                                                queue.add(new int[]{nr, nc});
+                                                                                                                                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                                                                                                                                                    }
 
-        return dist;
-    }
-}
+                                                                                                                                                                                                                                                                                                                                                                                                                                            return mat;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                }
