186a68b469ce8a52845500e74fc98ad8ae1a751a
Vinay-VTU <vtu25539@veltech.edu.in>
2026-05-09 22:28:05 +0530

    Time: 0 ms (100%), Space: 42.8 MB (37.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int rob(int[] nums) {
-        int prev2 = 0;
-        int prev1 = 0;
+        public int rob(int[] nums) {
+                int prev2 = 0;
+                        int prev1 = 0;
 
-        for (int money : nums) {
-            int take = money + prev2;
-            int skip = prev1;
+                                for (int money : nums) {
+                                            int robCurrent = prev2 + money;
+                                                        int skipCurrent = prev1;
 
-            int current = Math.max(take, skip);
+                                                                    int current = Math.max(robCurrent, skipCurrent);
 
-            prev2 = prev1;
-            prev1 = current;
-        }
+                                                                                prev2 = prev1;
+                                                                                            prev1 = current;
+                                                                                                    }
 
-        return prev1;
-    }
-}
+                                                                                                            return prev1;
+                                                                                                                }
+                                                                                                                }
