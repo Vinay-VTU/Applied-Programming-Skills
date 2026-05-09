2cfd2e7f408a1388cc30785007e99702ce71371c
Vinay-VTU <vtu25539@veltech.edu.in>
2026-05-09 22:10:31 +0530

    Time: 0 ms (100%), Space: 46.9 MB (24.59%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,30 +1,29 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int oldColor = image[sr][sc];
+        public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+                int oldColor = image[sr][sc];
 
-        if (oldColor == color) {
-            return image;
-        }
+                        if (oldColor == color) {
+                                    return image;
+                                            }
 
-        dfs(image, sr, sc, oldColor, color);
+                                                    dfs(image, sr, sc, oldColor, color);
 
-        return image;
-    }
+                                                            return image;
+                                                                }
 
-    private void dfs(int[][] image, int r, int c, int oldColor, int newColor) {
-        if (r < 0 || r >= image.length || c < 0 || c >= image[0].length) {
-            return;
-        }
+                                                                    private void dfs(int[][] image, int row, int col, int oldColor, int newColor) {
+                                                                            int m = image.length;
+                                                                                    int n = image[0].length;
 
-        if (image[r][c] != oldColor) {
-            return;
-        }
+                                                                                            if (row < 0 || row >= m || col < 0 || col >= n || image[row][col] != oldColor) {
+                                                                                                        return;
+                                                                                                                }
 
-        image[r][c] = newColor;
+                                                                                                                        image[row][col] = newColor;
 
-        dfs(image, r + 1, c, oldColor, newColor);
-        dfs(image, r - 1, c, oldColor, newColor);
-        dfs(image, r, c + 1, oldColor, newColor);
-        dfs(image, r, c - 1, oldColor, newColor);
-    }
-}
+                                                                                                                                dfs(image, row + 1, col, oldColor, newColor);
+                                                                                                                                        dfs(image, row - 1, col, oldColor, newColor);
+                                                                                                                                                dfs(image, row, col + 1, oldColor, newColor);
+                                                                                                                                                        dfs(image, row, col - 1, oldColor, newColor);
+                                                                                                                                                            }
+                                                                                                                                                            }
