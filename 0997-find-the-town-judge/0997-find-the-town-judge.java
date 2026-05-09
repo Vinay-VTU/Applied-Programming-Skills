540c2468066b4aa37873d99f0a787ce7bb16c36d
Vinay-VTU <vtu25539@veltech.edu.in>
2026-05-09 21:58:40 +0530

    Time: 3 ms (70.96%), Space: 53.8 MB (32.01%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,18 +1,22 @@
 class Solution {
-    public int findJudge(int n, int[][] trust) {
-        int[] score = new int[n + 1];
+        public int findJudge(int n, int[][] trust) {
+                int[] score = new int[n + 1];
 
-        for (int[] t : trust) {
-            score[t[0]]--;  // person who trusts someone cannot be judge
-            score[t[1]]++;  // person trusted by others
-        }
+                        for (int[] t : trust) {
+                                    int a = t[0];
+                                                int b = t[1];
 
-        for (int i = 1; i <= n; i++) {
-            if (score[i] == n - 1) {
-                return i;
-            }
-        }
+                                                            score[a]--;  // a trusts someone, so cannot be judge
+                                                                        score[b]++;  // b is trusted by someone
+                                                                                }
 
-        return -1;
-    }
+                                                                                        for (int i = 1; i <= n; i++) {
+                                                                                                    if (score[i] == n - 1) {
+                                                                                                                    return i;
+                                                                                                                                }
+                                                                                                                                        }
+
+                                                                                                                                                return -1;
+                                                                                                                                                    }
+                                                                                                                                                    
 }
