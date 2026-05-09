ee46dab4597ae5da09ad0b42eaa497a7de32436f
Vinay-VTU <vtu25539@veltech.edu.in>
2026-05-09 22:09:26 +0530

    Time: 3 ms (86.75%), Space: 52.1 MB (74.02%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,31 +1,34 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int count = 0;
+        public int numIslands(char[][] grid) {
+                int m = grid.length;
+                        int n = grid[0].length;
+                                int count = 0;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    count++;
-                    dfs(grid, i, j);
-                }
-            }
-        }
+                                        for (int i = 0; i < m; i++) {
+                                                    for (int j = 0; j < n; j++) {
+                                                                    if (grid[i][j] == '1') {
+                                                                                        count++;
+                                                                                                            dfs(grid, i, j);
+                                                                                                                            }
+                                                                                                                                        }
+                                                                                                                                                }
 
-        return count;
-    }
+                                                                                                                                                        return count;
+                                                                                                                                                            }
 
-    private void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
-            return;
-        }
+                                                                                                                                                                private void dfs(char[][] grid, int row, int col) {
+                                                                                                                                                                        int m = grid.length;
+                                                                                                                                                                                int n = grid[0].length;
 
-        grid[i][j] = '0';
+                                                                                                                                                                                        if (row < 0 || row >= m || col < 0 || col >= n || grid[row][col] == '0') {
+                                                                                                                                                                                                    return;
+                                                                                                                                                                                                            }
 
-        dfs(grid, i + 1, j);
-        dfs(grid, i - 1, j);
-        dfs(grid, i, j + 1);
-        dfs(grid, i, j - 1);
-    }
-}
+                                                                                                                                                                                                                    grid[row][col] = '0'; // mark as visited
+
+                                                                                                                                                                                                                            dfs(grid, row + 1, col);
+                                                                                                                                                                                                                                    dfs(grid, row - 1, col);
+                                                                                                                                                                                                                                            dfs(grid, row, col + 1);
+                                                                                                                                                                                                                                                    dfs(grid, row, col - 1);
+                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                        }
