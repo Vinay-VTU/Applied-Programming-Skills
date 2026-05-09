17b4e1f53fd2f399bef3c0bca594321ff68af780
Vinay-VTU <vtu25539@veltech.edu.in>
2026-05-09 22:33:17 +0530

    Time: 51 ms (93.86%), Space: 95.9 MB (9.4%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -2,18 +2,21 @@ import java.util.*;
 
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+            Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
 
-        int arrows = 1;
-        int arrowPos = points[0][1];
+                    int arrows = 1;
+                            int arrowPosition = points[0][1];
 
-        for (int i = 1; i < points.length; i++) {
-            if (points[i][0] > arrowPos) {
-                arrows++;
-                arrowPos = points[i][1];
-            }
-        }
+                                    for (int i = 1; i < points.length; i++) {
+                                                int start = points[i][0];
+                                                            int end = points[i][1];
 
-        return arrows;
-    }
-}
+                                                                        if (start > arrowPosition) {
+                                                                                        arrows++;
+                                                                                                        arrowPosition = end;
+                                                                                                                    }
+                                                                                                                            }
+
+                                                                                                                                    return arrows;
+                                                                                                                                        }
+                                                                                                                                        }
