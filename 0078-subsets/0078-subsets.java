f2029940fb421cb5b1ea7854038d309c70c0fe9c
Vinay-VTU <vtu25539@veltech.edu.in>
2026-05-09 22:34:31 +0530

    Time: 1 ms (88.93%), Space: 44.2 MB (38.55%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,25 +2,18 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(nums, 0, new ArrayList<>(), result);
-        return result;
-    }
+            List<List<Integer>> result = new ArrayList<>();
+                    backtrack(nums, 0, new ArrayList<>(), result);
+                            return result;
+                                }
 
-    private void backtrack(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
-        if (index == nums.length) {
-            result.add(new ArrayList<>(current));
-            return;
-        }
+                                    private void backtrack(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
+                                            result.add(new ArrayList<>(current));
 
-        // Do not take current number
-        backtrack(nums, index + 1, current, result);
-
-        // Take current number
-        current.add(nums[index]);
-        backtrack(nums, index + 1, current, result);
-
-        // Remove for backtracking
-        current.remove(current.size() - 1);
-    }
-}
+                                                    for (int i = index; i < nums.length; i++) {
+                                                                current.add(nums[i]);
+                                                                            backtrack(nums, i + 1, current, result);
+                                                                                        current.remove(current.size() - 1);
+                                                                                                }
+                                                                                                    }
+                                                                                                    }
