006beb23f833e38878998cac6b625144f0e7bedb
Vinay-VTU <vtu25539@veltech.edu.in>
2026-05-09 22:23:01 +0530

    Time: 0 ms (100%), Space: 42.4 MB (68.17%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,13 +1,13 @@
-class Solution {
-    public int reverseBits(int n) {
-        int result = 0;
+public class Solution {
+        public int reverseBits(int n) {
+                int result = 0;
 
-        for (int i = 0; i < 32; i++) {
-            result = result << 1;      // make space for next bit
-            result = result | (n & 1); // add last bit of n
-            n = n >>> 1;              // unsigned right shift
-        }
+                        for (int i = 0; i < 32; i++) {
+                                    result = result << 1;
+                                                result = result | (n & 1);
+                                                            n = n >>> 1;
+                                                                    }
 
-        return result;
-    }
-}
+                                                                            return result;
+                                                                                }
+                                                                                }
